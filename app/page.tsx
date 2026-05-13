5931c9e25493daad45e223a90d3ef0fe857e1b38
RosaaQC <rquispe8@autonoma.edu.pe>
2026-05-13 16:06:12 -0500

    P2

--- app/page.tsx
+++ app/page.tsx
@@ -586,8 +586,8 @@ function Footer() {
             Contacto
           </h4>
           <ul className="text-sm space-y-1.5 text-neutral-300">
-            <li>WhatsApp: 959 560 616</li>
-            <li>SJM, Limaaaaa</li>
+            <li>WhatsApp: 985 577 017</li>
+            <li>San Juan de Miraflores, Lima</li>
           </ul>
         </div>
         <div>
